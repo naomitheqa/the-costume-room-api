f909b0226d01f6c6f0212dac2dcbb1bc6deeecf0
Naomi Benjamin <naomi.benjamin@gkco.com>
2023-10-27 11:26:22 -0500

    update: updated seed files

--- src/db/seeders/20231027160618-add-admins.js
+++ src/db/seeders/20231027160618-add-admins.js
@@ -1,11 +1,13 @@
 "use strict";
 const bcrypt = require("bcrypt");
+const { v4: uuidv4 } = require("uuid");
 
 module.exports = {
   async up(queryInterface, Sequelize) {
     const hash = await bcrypt.hash("GodFirst", 10);
     await queryInterface.bulkInsert("users", [
       {
+        hashid: uuidv4(),
         firstName: "Naomi",
         lastName: "Silvera",
         email: "nobenjamin19@gmail.com",
