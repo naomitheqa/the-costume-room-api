6f65632726681a40045ccf1d1c7b9dafe943e904
Naomi Benjamin <naomi.benjamin@gkco.com>
2023-11-03 09:58:40 -0500

    chore: prevent getting /favicon.io

--- src/index.js
+++ src/index.js
@@ -20,7 +20,10 @@ app.get("/", (req, res, next) => {
   res.json({ message: "Welcome to The Costume Room API..." });
 });
 
-app.get('/favicon.ico', (req, res) => res.status(204));
+app.get('/favicon.ico', function(req, res) { 
+  res.status(204);
+  res.end();    
+});
 
 app.listen(8080, () => {
   console.log(`Server is running`);
