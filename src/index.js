434f8fc885163e00620414cb0a5d2aed25454681
Naomi Silvera <nobenjamin19@gmail.com>
2024-02-01 23:36:49 -0500

    chore: update prod config

--- src/index.js
+++ src/index.js
@@ -21,7 +21,7 @@ app.get("/", (req, res, next) => {
 });
 
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running`);
+  console.log(`Server is running on port ${process.env.PORT}`);
 });
 
 export default app;
