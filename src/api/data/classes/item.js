41d41efae2c89209712e4e9a9e64a411efc27537
naomitheqa <nobenjamin19@gmail.com>
2024-11-07 11:35:06 -0500

    add

---
+++ src/api/data/classes/item.js
@@ -0,0 +1,10 @@
+export default class Item {
+  constructor(id, name, description, count, status, group) {
+    this.id = id;
+    this.name = name;
+    this.description = description;
+    this.count = count;
+    this.status = status;
+    this.group = group;
+  }
+}
